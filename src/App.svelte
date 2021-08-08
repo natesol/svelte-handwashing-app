595aa8e2ea3a908777d7839660d8bfe11dbbff94
natesol <natesoln@gmail.com>
2021-08-08 04:17:33 +0300

    css dependencies working

--- src/App.svelte
+++ src/App.svelte
@@ -9,5 +9,6 @@
 
 
 <main>
-    <h1>Hello Svelte!</h1>
+    <h1>test</h1>
+    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Magnam nisi, quidem accusamus, consectetur maxime cumque quasi tempora saepe illo iure dolore autem ipsum doloribus sit sint placeat harum ad quod.</p>
 </main>
